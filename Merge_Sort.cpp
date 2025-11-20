509fa179d32e487c84177e36347f1bac4ffc5490
Princeag1652 <agarwalprince819>
2025-11-20 20:43:48 +0530

    Practical 7

---
+++ Merge_Sort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+void mergeArrays(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;  // size of left part
+    int n2 = right - mid;     // size of right part
+
+
+    int L[n1], R[n2];
+
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);      // left half
+        mergeSort(arr, mid + 1, right); // right half
+        mergeArrays(arr, left, mid, right); // merge
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
